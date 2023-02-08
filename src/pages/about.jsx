c54a239e7f440395a698df4f584c9bd6b06bcaae
Olive <olivelliott48@gmail.com>
2023-02-07 20:46:34 -0500

    Update about section

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -41,10 +41,10 @@ export default function About() {
   return (
     <>
       <Head>
-        <title>About - Spencer Sharp</title>
+        <title>About - Olivia Elliott</title>
         <meta
           name="description"
-          content="I’m Spencer Sharp. I live in New York City, where I design the future."
+          content="I'm Olivia Elliott. I'm a forward thinking full stack developer with a love for learning."
         />
       </Head>
       <Container className="mt-16 sm:mt-32">
@@ -61,58 +61,59 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              I’m Spencer Sharp. I live in New York City, where I design the
-              future.
+            I'm Olivia Elliott. I'm a forward thinking full stack developer 
+            with a knack for learning.
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
-                I’ve loved making things for as long as I can remember, and
-                wrote my first program when I was 6 years old, just two weeks
-                after my mom brought home the brand new Macintosh LC 550 that I
-                taught myself to type on.
+                As a kid, my head was always buried in a book or looking for something new to learn.
+                As I've grown, I've never lost my desire to challenge myself and continue
+                my education throughout my life. This led me into the world of coding, where I knew
+                that the possibilites of technology are endless. 
               </p>
               <p>
-                The only thing I loved more than computers as a kid was space.
-                When I was 8, I climbed the 40-foot oak tree at the back of our
-                yard while wearing my older sister’s motorcycle helmet, counted
-                down from three, and jumped — hoping the tree was tall enough
-                that with just a bit of momentum I’d be able to get to orbit.
+                I decided to attend the UNC Chapel Hill Full Stack Coding Bootcamp
+                in April of 2022; completing a 6 month intensive program focused 
+                on gaining technical expertise. I learned how to develop interactive web 
+                applications that utilize UI, front-end, and back-end technologies. 
+                I attended 450+ hours of live instructional time, along with working on
+                asynchronous material, group projects, and individual assignments. I continued
+                my work as a full time Operations Manager throughout the program which allowed
+                me to perfect my time management skills. I'm ready and excited to shift my focus into the
+                world of technology and contribute to the community in any way that I can.
               </p>
               <p>
-                I spent the next few summers indoors working on a rocket design,
-                while I recovered from the multiple surgeries it took to fix my
-                badly broken legs. It took nine iterations, but when I was 15 I
-                sent my dad’s Blackberry into orbit and was able to transmit a
-                photo back down to our family computer from space.
+                My culminating work experience of the past 10 years has allowed me to
+                confidently and quickly become an innovative contributor to any team and business.
+                It has sharpened my skills in multitasking, business management, project management,
+                client communications, problem solving, marketing, critical thinking, and efficiently 
+                completing day to day operations for the overall growth and improvement of the 
+                business. These skills have brought me into my current work position which most
+                clearly portrays my growth as a determined individual with a strong work ethic.
               </p>
               <p>
-                Today, I’m the founder of Planetaria, where we’re working on
-                civilian space suits and manned shuttle kits you can assemble at
-                home so that the next generation of kids really <em>can</em>{' '}
-                make it to orbit — from the comfort of their own backyards.
+                I know that I can be a top contributor to any team. If you give me the opportunity,
+                I will devote my time, passion, and upmost quality work to your project.
               </p>
             </div>
           </div>
           <div className="lg:pl-20">
             <ul role="list">
-              <SocialLink href="#" icon={TwitterIcon}>
+              <SocialLink href="https://twitter.com/oliv_e_lliott" icon={TwitterIcon}>
                 Follow on Twitter
               </SocialLink>
-              <SocialLink href="#" icon={InstagramIcon} className="mt-4">
-                Follow on Instagram
-              </SocialLink>
-              <SocialLink href="#" icon={GitHubIcon} className="mt-4">
+              <SocialLink href="https://github.com/olivelliott" icon={GitHubIcon} className="mt-4">
                 Follow on GitHub
               </SocialLink>
-              <SocialLink href="#" icon={LinkedInIcon} className="mt-4">
+              <SocialLink href="https://linkedin/in/olivelliott" icon={LinkedInIcon} className="mt-4">
                 Follow on LinkedIn
               </SocialLink>
               <SocialLink
-                href="mailto:spencer@planetaria.tech"
+                href="mailto:olivelliott48@gmail.com"
                 icon={MailIcon}
                 className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
               >
-                spencer@planetaria.tech
+                olivelliott48@gmail.com
               </SocialLink>
             </ul>
           </div>
